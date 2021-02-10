7bc239888c46c0a90dd5b6908abb2aaa06058a79
AshkhenArakelyan <ashkhenarakelyank@gmail.com>
2021-02-10 13:13:29 +0400

    Created service.js with Service class for all network calls data

--- src/containers/Posts/Posts.js
+++ src/containers/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Post from 'components/Post/Post';
+import service from 'api/service';
 
 import './Posts.scss';
 
@@ -9,22 +10,62 @@ class Posts extends Component {
     state = {
         posts: [],
     }
-
+   
     componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(res => res.json())
+        service.getAllPosts()
         .then(resJson => {
             this.setState({
-                posts: resJson
+                posts: resJson,
             })
         })
         .catch(err => {
-            console.log(err);
+
+        })
+    }
+    getPost = () => {
+        service.getPost(1)
+        .then(resJson => {
+            this.setState({
+                posts: [...this.state.posts, resJson] // two children with the same key
+            })
+        })
+        .catch(err => {
+
+        })
+    }
+    createPost = () => {
+        service.createPost({
+            title: 'My post title',
+            body: 'My post body',
+            userId: 1
+        })
+        .then(resJson => {
+            this.setState({
+                posts: [...this.state.posts, resJson]
+            })
+        })
+    }
+    updatePost = () => {
+        service.updatePost(1, { title: 'Updated Title' })
+        .then(resJson => {
+            const newPost = this.state.posts.map(elem => {
+                if(elem.id === resJson.id) {
+                    return resJson;
+                } else {
+                    return elem;
+                }
+            })
+            this.setState({
+                posts: newPost
+            })
         })
     }
     render() {
         return (
             <div className="app-posts">
+                <button onClick={this.getPost}>Get post</button>
+                <button onClick={this.createPost}>Create Post</button>
+                <button onClick={this.updatePost}>Update Post</button>
                 {
                     this.state.posts.map(post => {
                         return <Post 
